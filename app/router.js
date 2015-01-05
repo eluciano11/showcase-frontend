ee14b90d335ff0a460a494b25d82dfdc7cb50778
Emmanuel Luciano <emma.luciano11@gmail.com>
2015-01-05 15:48:56 -0400

    Added some other paths

--- app/router.js
+++ app/router.js
@@ -8,12 +8,20 @@ var Router = Ember.Router.extend({
 Router.map(function() {
 	this.resource('projects', function(){
 		this.route('add');
+		this.route('university', {path: '/:university_slug'});
+		this.route('universityDepartment', {path: '/:university_slug/:department_slug'});
+		this.route('department', {path: '/:department'});
+		this.route('specificProject', {path: '/:project_slug'})
 		this.route('edit', {path: '/:project_slug/edit'});
+		this.route('delete', {path: '/:project_slug/delete'});
 	});
 	this.resource('users', function(){
 		this.route('login');
 		this.route('logout');
 		this.route('register');
+		this.route('settings');
+		this.route('forgotPassword');
+		this.route('resetPassword');
 	});
 	this.route('search');
 	this.route('loading');
