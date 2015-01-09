9ad3046e08ff6ab892a161d4c5a51229dadb1dbe
Emmanuel Luciano <emma.luciano11@gmail.com>
2015-01-09 00:54:07 -0400

    removed unnecessary stuff in the route

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
-import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-export default DS.RESTAdapter.extend(ApplicationRouteMixin, {
+export default DS.RESTAdapter.extend({
 	host: 'http://localhost:8000',
 	namespace: 'api',
 	headers: {
