9778945a0a482965efde90e3b7deaf5835dbc3d5
Emmanuel Luciano <emma.luciano11@gmail.com>
2015-01-15 23:02:37 -0400

    all the deletions

--- app/routes/users/edit/specific/change-password.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-	model: function(){
-		var slug = this.modelFor('users.edit.specific').user_slug;
-		return this.store.find('user', slug);
-	}
-});
