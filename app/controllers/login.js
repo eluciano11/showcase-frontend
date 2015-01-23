384d2ac634bca1ee77cb1eb5690d1ae62d474707
Emmanuel Luciano <emma.luciano11@gmail.com>
2015-01-23 00:22:49 -0400

    added register template and controller

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
 export default Ember.Controller.extend(LoginControllerMixin, {
-	authenticator: 'simple-auth-authenticator:token'
+	successfullyCreated: false,
+	authenticator: 'simple-auth-authenticator:token',
+	actions: {
+		dismiss: function(){
+			this.get('successfullyCreated',false);
+		}
+	}
 });
