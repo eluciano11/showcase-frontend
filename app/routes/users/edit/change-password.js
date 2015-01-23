e8b184d1c8fb10df7a6199d0d5576d9051cdf60b
Emmanuel Luciano <emma.luciano11@gmail.com>
2015-01-23 18:13:52 -0400

    added changes password and made some other changes

--- app/routes/users/edit/change-password.js
+++ app/routes/users/edit/change-password.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model: function(){
-		var id = this.modelFor('users.edit').user_id;
-		return this.store.find('user', id);
-	}
+	// model: function(){
+	// 	var id = this.modelFor('users.edit').id;
+	// 	return this.store.find('user', id);
+	// }
 });
