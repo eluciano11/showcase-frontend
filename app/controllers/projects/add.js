caa9b6cee5fe8d0aee056dd2aaa2ab734554c94d
Emmanuel Luciano <emma.luciano11@gmail.com>
2015-01-27 10:35:27 -0400

    project now uploads images

--- app/controllers/projects/add.js
+++ app/controllers/projects/add.js
@@ -4,14 +4,28 @@ export default Ember.Controller.extend({
 	actions: {
 		add: function(){
 			var data = this.getProperties('title', 'summary', 'university', 'department', 'story');
-			var project = this.store.createRecord('project', {
-				title: data.title,
-				summary: data.summary,
-				university: data.university,
-				department: data.department,
-				story: data.story
+			var fileUploaded = document.getElementById('file').files[0];
+			
+			var formData = new FormData();
+			formData.append('title', data.title);
+			formData.append('summary', data.summary);
+			formData.append('university', data.university);
+			formData.append('department', data.department);
+			formData.append('story', data.story);
+			formData.append('screenshot', fileUploaded);
+
+			var self = this;
+
+			Ember.$.ajax({
+				url: 'http://localhost:8000/api/projects',
+				type: 'POST',
+				data: formData,
+				processData: false,  // tell jQuery not to process the data
+  				contentType: false   // tell jQuery not to set contentType
+			}).then(function(response){
+				self.store.find('project', response.id);
+				self.transitionToRoute('projects.specific', response.id);
 			});
-			project.save();
 		},
 		dismiss: function(){
 			this.set('hasFailed', false);
