f98dde142af75dad6f92e866970059a8127d53ef
Emmanuel Luciano <emma.luciano11@gmail.com>
2015-01-28 20:54:34 -0400

    fixed undefine error on viewing all the projects

--- app/routes/projects/add.js
+++ app/routes/projects/add.js
@@ -4,9 +4,6 @@ export default Ember.Route.extend({
 	beforeModel: function(){
 		this.controllerFor('application').set('inAddProject', true);
 	},
-	model: function(){
-		return this.store.createRecord('project');
-	},
 	setupController: function(controller, model){
 		controller.set('departments', this.store.find('department'));
 		controller.set('universities', this.store.find('university'));
