a4853b615e6164fddb536c563c8ae17c24b64abb
Emmanuel Luciano <emma.luciano11@gmail.com>
2015-02-13 22:39:17 -0400

    added university

--- app/routes/universities/specific.js
+++ app/routes/universities/specific.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function(params){
-		return this.store.find('university', params.university_id);
+		this.set('id', params.university_id);
+		return this.store.find('project', { university: params.university_id });
+	},
+	setupController: function(controller, model){
+		controller.set('university', this.store.find('university', this.get('id')));
+		controller.set('projects', model);
 	}
 });
